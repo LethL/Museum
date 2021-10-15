19f0e3b39cb7068d97262fe50e49be0c626cde5e
Leth4L <carribean_08@mail.ru>
2021-10-15 09:42:56 +0300

    feat:add my project from rss school task

--- museum/app.js
+++
@@ -1,8 +0,0 @@
-const progress = document.querySelectorAll('.progress');
-
-progress.forEach(e => {
-    e.addEventListener('input', function() {
-        const value = this.value;
-        this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #fff ${value}%, white 100%)`
-    });
-});
